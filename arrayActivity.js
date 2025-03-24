e12cbb2fdeab60d43e596d8867ddf4134664fa47
Faith <stephjavinar@gmail.com>
2025-03-24 09:56:50 -0700

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,15 @@
+//Name: Javinar, Stephanie Faith D. 
+//Create an array and store your three favorite singers in it: Taylor Swift, Jennie KIm, Niki
+let favSingers = ["Taylor Swift", "Jennie Kim", "Niki"];
+//Using the index notation log the first singer 
+console.log(favSingers[0]);
+//Create an array called favNumbers and store four of your favorite numbers: 13,22,14,11
+let favNumbers = ["13", "22", "14", "11"];
+//Create an array named mixedArr and store different data types  ["string", ["otherarray"], 123, true]
+let mixedArr = ["Serena",["Lily of the Valley"], 13, true];
+//Access and log each item in the mixedArr using array index notation.
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1][0]); 
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
